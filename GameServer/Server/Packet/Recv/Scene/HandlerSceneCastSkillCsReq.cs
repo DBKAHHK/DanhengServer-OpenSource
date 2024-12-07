70a83ede707974a6bea0679c0cebd340912dedc3
StopWuyu <wuyustop@outlook.com>
2024-12-07 17:05:40 +0800

    fix: trailblazer couldnt use skill correctly

--- GameServer/Server/Packet/Recv/Scene/HandlerSceneCastSkillCsReq.cs
+++ GameServer/Server/Packet/Recv/Scene/HandlerSceneCastSkillCsReq.cs
@@ -1,4 +1,5 @@
-﻿using EggLink.DanhengServer.Data.Config;
+﻿using EggLink.DanhengServer.Data;
+using EggLink.DanhengServer.Data.Config;
 using EggLink.DanhengServer.GameServer.Game.Battle.Skill;
 using EggLink.DanhengServer.GameServer.Server.Packet.Send.Scene;
 using EggLink.DanhengServer.Kcp;
@@ -38,7 +39,10 @@ public class HandlerSceneCastSkillCsReq : Handler
                 if (req.SkillIndex > 0)
                 {
                     // Cast skill effects
-                    if (caster.AvatarInfo.Excel != null && caster.AvatarInfo.Excel!.MazeSkill != null)
+                    var excel = caster.AvatarInfo.PathId > 0
+                        ? GameData.AvatarConfigData[caster.AvatarInfo.PathId]
+                        : caster.AvatarInfo.Excel;
+                    if (excel != null && excel.MazeSkill != null)
                     {
                         mazeSkill = MazeSkillManager.GetSkill(caster.AvatarInfo.GetAvatarId(), (int)req.SkillIndex,
                             req);
